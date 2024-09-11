90a8585cee7d4b439039eb548fc06275af95236b
RamyaKalaiyarasan <ramyakrishguna@gmail.com>
2024-09-11 17:06:01 +0530

    Kalai's code

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -3,7 +3,8 @@ package org.test;
 public class SampleTest {
 	public static void main(String[] arg)
 	{
-		System.out.println("Test1");
+		System.out.println("GIT Day 1");
+		System.out.println("GIT Day 2");
 		//here start to write implementations
 	}
 
